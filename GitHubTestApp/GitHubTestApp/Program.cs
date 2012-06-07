f572dd80adf9598771d5b6bef9b2660129bb4412
Rick Holwerda <rick.holwerda@gmail.com>
2012-06-07 14:51:27 +0200

    Project toegevoegd

---
+++ GitHubTestApp/GitHubTestApp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHubTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String woord = "Aartdejong";
+            Console.WriteLine(woord);
+            Console.WriteLine(Program.woordOmdraaien(woord));
+
+            Console.WriteLine("");
+
+            int getal = 999999999;
+            Console.WriteLine(getal);
+            Console.WriteLine("Som der cijfers: " + somDerCijfers(getal));
+            Console.Read();
+        }
+
+        public static int aantalWoordenInZin(String inputString)
+        {
+            string[] woorden = inputString.Split(' ');
+            return woorden.Length;
+        }
+
+        public static int aantalLettersInWoord(String input, char letter)
+        {
+            int output = 0;
+            foreach (char c in input.ToCharArray())
+            {
+                if (c == letter)
+                    output += 1;
+            }
+            return output;
+        }
+
+        public static String woordOmdraaien(String input)
+        {
+            String output = "";
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+            return output;
+        }
+
+        public static int somDerCijfers(int input)
+        {
+            int output = 0;
+
+            while (input != 0)
+            {
+                output += input % 10;
+                input /= 10;
+            }
+            if (output > 9)
+                output = somDerCijfers(output);
+
+            return output;
+        }
+
+        public static int optellen(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int aftrekken(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int kwadrateren(int a)
+        {
+            return a * a;
+        }
+
+        public static int vermenigvuldigen(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int machtsverheffen(int a, int b)
+        {
+            int output = 1;
+            for(int i = 0; i < b ; i++)
+            {
+                output *= a;
+            }
+            return output;
+        }
+    }
+}
---
+++ GitHubTestApp/TestProject1/ProgramTest.cs
@@ -0,0 +1,204 @@
+﻿using GitHubTestApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject1
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ProgramTest and is intended
+    ///to contain all ProgramTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProgramTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for woordOmdraaien
+        ///</summary>
+        [TestMethod()]
+        public void woordOmdraaienTest()
+        {
+            string input = "fiets";
+            string expected = "steif";
+            string actual;
+            actual = Program.woordOmdraaien(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for somDerCijfers
+        ///</summary>
+        [TestMethod()]
+        public void somDerCijfersTest()
+        {
+            int input = 12345;
+            int expected = 6;
+            int actual;
+            actual = Program.somDerCijfers(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for somDerCijfers
+        ///</summary>
+        [TestMethod()]
+        public void somDerCijfersTest2()
+        {
+            int input = 4;
+            int expected = 4;
+            int actual;
+            actual = Program.somDerCijfers(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for vermenigvuldigen
+        ///</summary>
+        [TestMethod()]
+        public void vermenigvuldigenTest()
+        {
+            int a = 2;
+            int b = 3;
+            int expected = 6;
+            int actual;
+            actual = Program.vermenigvuldigen(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for optellen
+        ///</summary>
+        [TestMethod()]
+        public void optellenTest()
+        {
+            int a = 10;
+            int b = 5;
+            int expected = 15;
+            int actual;
+            actual = Program.optellen(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for kwadrateren
+        ///</summary>
+        [TestMethod()]
+        public void kwadraterenTest()
+        {
+            int a = 5;
+            int expected = 25;
+            int actual;
+            actual = Program.kwadrateren(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for aftrekken
+        ///</summary>
+        [TestMethod()]
+        public void aftrekkenTest()
+        {
+            int a = 28;
+            int b = 7;
+            int expected = 21;
+            int actual;
+            actual = Program.aftrekken(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for machtsverheffen
+        ///</summary>
+        [TestMethod()]
+        public void machtsverheffenTest()
+        {
+            int a = 2;
+            int b = 5;
+            int expected = 32;
+            int actual;
+            actual = Program.machtsverheffen(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        /// <summary>
+        ///A test for aantalLettersInWoord
+        ///</summary>
+        [TestMethod()]
+        public void aantalLettersInWoordTest()
+        {
+            string input = "hottentottententententoonstellingstereinen";
+            char letter = 't';
+            int expected = 11;
+            int actual;
+            actual = Program.aantalLettersInWoord(input, letter);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for aantalWoordenInZin
+        ///</summary>
+        [TestMethod()]
+        public void aantalWoordenInZinTest()
+        {
+            string inputString = "Omdat ik boos ben";
+            int expected = 4;
+            int actual;
+            actual = Program.aantalWoordenInZin(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
